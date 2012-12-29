f180507f39db93039d54609bea39b4bfef18b850
kyr0 <info@aron-homberg.de>
2012-12-29 14:02:52 +0100

    Backward compatibility

--- bin/port-vhoster.js
+++ bin/port-vhoster.js
@@ -2,13 +2,14 @@
 var fs = require('fs'),
     path = require('path'),
     findup = require('../lib/findup'),
+    existsSync = fs.existsSync || path.existsSync,
     dir;
 
 // Where might a locally-installed port-vhoster?
 dir = path.resolve(findup(process.cwd(), 'port-vhoster.js'), '../node_modules/port-vhoster');
 
 // If port-vhoster is installed locally, use it. Otherwise use this port-vhoster.
-if (!fs.existsSync(dir)) {
+if (!existsSync(dir)) {
     dir = '../lib/port-vhoster';
 }
 
